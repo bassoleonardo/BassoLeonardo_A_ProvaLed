c18fb66f11b82f8769277978dea6784466e1dc3b
leonardo <leonardo.basso@barsanti.edu.it>
2019-02-02 10:39:49 +0100

    02/02/2019 - PRIMO CARICAMENTO

---
+++ SEMAFORO/SEMAFORO.ino
@@ -0,0 +1,63 @@
+int led1 = 12;
+int led2 = 11;
+int led3 = 10;
+int led4 = 7;
+int led5 = 6;
+int led6 = 5;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT); 
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+  pinMode(led5, OUTPUT);
+  pinMode(led6, OUTPUT);
+}
+  void lampeggio(){
+    for(int i=0; i<4; i++){
+    digitalWrite(led3, HIGH);
+    delay(500);
+    digitalWrite(led3, LOW);
+    delay(500);
+    }
+  }
+  void lampeggio2(){
+    for(int i=0; i<4; i++){
+      digitalWrite(led6, HIGH);
+      delay(500);
+      digitalWrite(led6, LOW);
+      delay(500);
+    }
+  }
+void loop() {
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, LOW);
+  digitalWrite(led5, LOW);
+  digitalWrite(led6, HIGH);
+  delay(2000);
+  lampeggio2();
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, LOW);
+  digitalWrite(led5, LOW);
+  digitalWrite(led6, HIGH);
+  delay(2000); 
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, HIGH);
+  digitalWrite(led4, HIGH);
+  digitalWrite(led5, LOW);
+  digitalWrite(led6, LOW);
+  delay(2000);
+  lampeggio();
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, LOW); 
+  digitalWrite(led5, LOW);
+  digitalWrite(led6, HIGH);
+  delay(2000); 
+}
