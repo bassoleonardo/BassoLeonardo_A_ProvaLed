10073998e32a5abe72f5531f28774ae361513ba3
leonardo <leonardo.basso@barsanti.edu.it>
2019-02-02 11:37:12 +0100

    02/02/2019

--- SEMAFORO/SEMAFORO.ino
+++ SEMAFORO/SEMAFORO.ino
@@ -4,6 +4,8 @@ int led3 = 10;
 int led4 = 7;
 int led5 = 6;
 int led6 = 5;
+String imput;
+int nlampeggi;
 void setup() {
   // put your setup code here, to run once:
   pinMode(led1, OUTPUT);
@@ -12,24 +14,35 @@ void setup() {
   pinMode(led4, OUTPUT);
   pinMode(led5, OUTPUT);
   pinMode(led6, OUTPUT);
+  Serial.begin(9600);
+  Serial.println("seliale attivata");
+  String imput = Serial.readString();
+  int nlampeggi;
 }
   void lampeggio(){
-    for(int i=0; i<4; i++){
-    digitalWrite(led3, HIGH);
-    delay(500);
+    for(int i=0; i<nlampeggi; i++){
     digitalWrite(led3, LOW);
+    delay(500);
+    digitalWrite(led3, HIGH);
     delay(500);
     }
   }
   void lampeggio2(){
-    for(int i=0; i<4; i++){
-      digitalWrite(led6, HIGH);
-      delay(500);
+    for(int i=0; i<nlampeggi; i++){
       digitalWrite(led6, LOW);
+      delay(500);
+      digitalWrite(led6, HIGH);
       delay(500);
     }
   }
+
 void loop() {
+  if (Serial.available() > 0){
+      Serial.print("quanti lampeggi vuoi?");
+      imput = Serial.read();
+      nlampeggi = imput.toInt();
+    }
+  while(nl
   digitalWrite(led1, HIGH);
   digitalWrite(led2, LOW);
   digitalWrite(led3, LOW);
