8c3ab8aa392488d5c6d1128788d9b338df07b5c0
leonardo <leonardo.basso@barsanti.edu.it>
2019-02-02 15:24:17 +0100

    02/02/2019 - accertarsi che il programma funzioni correttamente (o per il suo 90%)

--- SEMAFORO/SEMAFORO.ino
+++ SEMAFORO/SEMAFORO.ino
@@ -1,10 +1,10 @@
-int led1 = 12;
-int led2 = 11;
-int led3 = 10;
-int led4 = 7;
-int led5 = 6;
-int led6 = 5;
-String imput;
+int led1 = 12; // ROSSO
+int led2 = 11; // GALLO
+int led3 = 10; // VERDE
+int led4 = 7;  // ROSSO
+int led5 = 6;  // GIALLO 
+int led6 = 5;  // VERDE
+String input;
 int nlampeggi;
 void setup() {
   // put your setup code here, to run once:
@@ -15,34 +15,17 @@ void setup() {
   pinMode(led5, OUTPUT);
   pinMode(led6, OUTPUT);
   Serial.begin(9600);
-  Serial.println("seriale attivata");
-  imput = Serial.readString();
+  Serial.println("Seriale attivata");
+  input = Serial.readString();
 }
-  void lampeggio(){
-    for(int i=0; i<nlampeggi; i++){
-    digitalWrite(led3, LOW);
-    delay(200);
-    digitalWrite(led3, HIGH);
-    delay(200);
-    }
-  }
-  void lampeggio2(){
-    for(int i=0; i<nlampeggi; i++){
-      digitalWrite(led6, LOW);
-      delay(200);
-      digitalWrite(led6, HIGH);
-      delay(200);
-    }
-  }
-
 void loop() {
-  if (Serial.available() > 0){
-      Serial.print("quanti lampeggi vuoi?");
-      imput = Serial.readString();
-      nlampeggi = imput.toInt();
-      while(Serial.available() == 0);
+  if (Serial.available() > 0){ // questo comando verifica se la porta seriale è disponibile o meno...
+      Serial.println("Quanti lampeggi vuoi?");
+      input = Serial.readString();
+      nlampeggi = input.toInt();
+      while(nlampeggi == 0); // condizione vera per ripetere il ciclo per poi proseguire con l'inserimento dei valori...
       Serial.println(nlampeggi);
-    }
+  }
   digitalWrite(led1, HIGH);
   digitalWrite(led2, LOW);
   digitalWrite(led3, LOW);
@@ -66,11 +49,28 @@ void loop() {
   digitalWrite(led6, LOW);
   delay(2000);
   lampeggio();
-  digitalWrite(led1, HIGH);
+  digitalWrite(led1, LOW);
   digitalWrite(led2, HIGH);
   digitalWrite(led3, LOW);
-  digitalWrite(led4, LOW); 
+  digitalWrite(led4, HIGH); 
   digitalWrite(led5, HIGH);
   digitalWrite(led6, LOW);
   delay(2000); 
 }
+// ------- metodi personalizzati ------------ //
+  void lampeggio(){
+    for(int i=0; i<nlampeggi; i++){
+    digitalWrite(led3, LOW);
+    delay(200);
+    digitalWrite(led3, HIGH);
+    delay(200);
+    }
+  }
+  void lampeggio2(){
+    for(int i=0; i<nlampeggi; i++){
+      digitalWrite(led6, LOW);
+      delay(200);
+      digitalWrite(led6, HIGH);
+      delay(200);
+    }
+  }
